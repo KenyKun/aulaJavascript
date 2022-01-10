69ccffbd81c495bc9a7fa795caa8dda6261fe030
KenyKun <83681314+KenyKun@users.noreply.github.com>
2022-01-10 00:02:51 -0300

    2.0

---
+++ JavaScript DIO/js/main.js
@@ -0,0 +1,10 @@
+var nome = "Kenny Arromba Buracos";
+var n1 = 16;
+var n2 = 10;
+var frase = "Comi o cu da tropa inteira"
+//alert(nome + " tem " + idade + " anos de arrombamento de buracos");
+//alert(idade + idade2);
+console.log(nome);
+console.log(n1 + n2);
+console.log(frase.toUpperCase());
+//alert(frase.replace("cu", "forévis"))
